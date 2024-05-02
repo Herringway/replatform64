474f303975fbb731d4567d60e6653d954c0fdd52
Cameron Ross <elpenguino@gmail.com>
2024-05-02 15:33:51 -0300

    add backend.common.loading to package

--- source/librehome/package.d
+++ source/librehome/package.d
@@ -1,6 +1,7 @@
 module librehome;
 public import librehome.assets;
 public import librehome.backend.common.interfaces;
+public import librehome.backend.common.loading;
 public import librehome.common;
 public import librehome.planet;
 public import librehome.ui;
