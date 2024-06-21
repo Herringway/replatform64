c5139e6c3d257835f1805d10f2b925aa9a2b24cf
Cameron Ross <elpenguino@gmail.com>
2024-06-20 22:28:25 -0300

    add def for the add file func

--- source/replatform64/planet.d
+++ source/replatform64/planet.d
@@ -17,7 +17,8 @@ struct Progress {
 }
 
 alias ProgressUpdateFunction = void delegate(scope const Progress);
-alias ExtractFunction = void function(scope void delegate(string, const ubyte[]), scope ProgressUpdateFunction, immutable(ubyte)[]);
+alias AddFileFunction = void delegate(string, const ubyte[]);
+alias ExtractFunction = void function(scope AddFileFunction, scope ProgressUpdateFunction, immutable(ubyte)[]);
 alias LoadFunction = void function(const scope char[], const scope ubyte[], scope PlatformBackend);
 
 struct PlanetArchive {
