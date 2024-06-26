03dc0a3c65df8603bc3afca7eba023fe5e066c6d
Cameron Ross <elpenguino@gmail.com>
2024-06-26 13:35:02 -0300

    simplify Array2D.opApply

--- source/replatform64/util.d
+++ source/replatform64/util.d
@@ -155,27 +155,8 @@ struct Array2D(E) {
 			sink.formattedWrite!"%s\n"(this[0 .. $, row]);
 		}
 	}
-	int opApply(scope int delegate(size_t x, size_t y, ref E element) @safe pure dg) {
-		return opApplyImpl(dg);
-	}
-	int opApply(scope int delegate(size_t x, size_t y, ref E element) dg) {
-		return opApplyImpl(dg);
-	}
-	int opApply(scope int delegate(size_t x, size_t y, E element) dg) const {
-		return opApplyImpl(dg);
-	}
-	private int opApplyImpl(DG)(scope DG dg) {
-		foreach (iterY; 0 .. height) {
-			foreach (iterX, ref elem; this[0 .. $, iterY][]) {
-				auto result = dg(iterX, iterY, elem);
-				if (result) {
-					return result;
-				}
-			}
-		}
-		return 0;
-	}
-	private int opApplyImpl(DG)(scope DG dg) const {
+	alias opApply = opApplyImpl!(int delegate(size_t x, size_t y, E element));
+	int opApplyImpl(DG)(scope DG dg) {
 		foreach (iterY; 0 .. height) {
 			foreach (iterX, ref elem; this[0 .. $, iterY][]) {
 				auto result = dg(iterX, iterY, elem);
