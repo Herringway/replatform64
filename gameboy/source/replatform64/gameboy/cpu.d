6984e33e60f510bd70088015a6c6e12463345f0c
Cameron Ross <elpenguino@gmail.com>
2025-01-05 14:14:56 -0400

    add some Z80 opcode helpers

---
+++ gameboy/source/replatform64/gameboy/cpu.d
@@ -0,0 +1,94 @@
+module replatform64.gameboy.cpu;
+
+alias rra = rr;
+ubyte rr(ubyte value, ref bool carry) @safe pure {
+	const inCarry = carry;
+	carry = !!(value & 0b00000001);
+	value >>= 1;
+	value |= (inCarry << 7);
+	return value;
+}
+///
+@safe pure unittest {
+	runTest!rr(0b00000000, false, 0b00000000, false);
+	runTest!rr(0b00000000, true, 0b10000000, false);
+	runTest!rr(0b00000001, false, 0b00000000, true);
+	runTest!rr(0b00000010, false, 0b00000001, false);
+	runTest!rr(0b00000011, false, 0b00000001, true);
+}
+
+ubyte sra(ubyte value, ref bool carry) @safe pure {
+	carry = !!(value & 0b00000001);
+	const msb = value & 0b10000000;
+	value >>= 1;
+	value |= msb;
+	return value;
+}
+///
+@safe pure unittest {
+	runTest!sra(0b00000000, false, 0b00000000, false);
+	runTest!sra(0b10000000, false, 0b11000000, false);
+	runTest!sra(0b00000000, true, 0b00000000, false);
+	runTest!sra(0b10000000, true, 0b11000000, false);
+	runTest!sra(0b00000001, false, 0b00000000, true);
+	runTest!sra(0b00000010, false, 0b00000001, false);
+	runTest!sra(0b00000011, false, 0b00000001, true);
+}
+ubyte srl(ubyte value, ref bool carry) @safe pure {
+	carry = !!(value & 0b00000001);
+	value >>= 1;
+	return value;
+}
+///
+@safe pure unittest {
+	runTest!srl(0b00000000, false, 0b00000000, false);
+	runTest!srl(0b10000000, false, 0b01000000, false);
+	runTest!srl(0b00000000, true, 0b00000000, false);
+	runTest!srl(0b10000000, true, 0b01000000, false);
+	runTest!srl(0b00000001, false, 0b00000000, true);
+	runTest!srl(0b00000010, false, 0b00000001, false);
+	runTest!srl(0b00000011, false, 0b00000001, true);
+}
+
+alias rla = rl;
+ubyte rl(ubyte value, ref bool carry) @safe pure {
+	const inCarry = carry;
+	carry = !!(value & 0b10000000);
+	value <<= 1;
+	value |= inCarry;
+	return value;
+}
+///
+@safe pure unittest {
+	runTest!rl(0b00000000, false, 0b00000000, false);
+	runTest!rl(0b00000000, true, 0b00000001, false);
+	runTest!rl(0b00000001, false, 0b00000010, false);
+	runTest!rl(0b01000000, false, 0b10000000, false);
+	runTest!rl(0b11000000, false, 0b10000000, true);
+}
+ubyte sla(ubyte value, ref bool carry) @safe pure {
+	carry = !!(value & 0b10000000);
+	value <<= 1;
+	return value;
+}
+///
+@safe pure unittest {
+	runTest!sla(0b00000000, false, 0b00000000, false);
+	runTest!sla(0b00000000, true, 0b00000000, false);
+	runTest!sla(0b00000001, false, 0b00000010, false);
+	runTest!sla(0b01000000, false, 0b10000000, false);
+	runTest!sla(0b11000000, false, 0b10000000, true);
+}
+
+version(unittest) {
+	private static void runTest(alias func)(ubyte input, bool inCarry, ubyte expectedOutput, bool expectedCarry, string file = __FILE__, ulong line = __LINE__) {
+		import core.exception : AssertError;
+		bool carry = inCarry;
+		if (func(input, carry) != expectedOutput) {
+			throw new AssertError("Assertion failure (value)", file, line);
+		}
+		if (carry != expectedCarry) {
+			throw new AssertError("Assertion failure (carry)", file, line);
+		}
+	}
+}
