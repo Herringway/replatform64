40f089ac1526110a5c2268195bc527df4bd52cbc
Cameron Ross <elpenguino@gmail.com>
2025-02-18 17:43:57 -0400

    snes: import BGR555 properly

--- snes/source/replatform64/snes/package.d
+++ snes/source/replatform64/snes/package.d
@@ -5,3 +5,4 @@ public import replatform64;
 public import replatform64.snes.audio;
 public import replatform64.snes.hardware;
 public import replatform64.snes.platform;
+public import tilemagic.colours.formats : BGR555;
