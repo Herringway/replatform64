4aa9d5b14e99d7bc4aa105984e02491605b54d7f
Cameron Ross <elpenguino@gmail.com>
2025-02-26 23:55:12 -0400

    SDL3: implement support for WAV playback

--- source/replatform64/backend/sdl3/audio.d
+++ source/replatform64/backend/sdl3/audio.d
@@ -4,6 +4,7 @@ import replatform64.backend.common;
 import replatform64.backend.sdl3.common;
 import replatform64.dumping;
 
+import std.algorithm.comparison;
 import std.logger;
 import std.string;
 
@@ -19,7 +20,11 @@ class SDL3Audio : AudioBackend {
 	private uint sampleRate;
 	private uint channels;
 	private uint samples;
-	AudioCallbackInfo* callbackInfo;
+	private AudioCallbackInfo* callbackInfo;
+	private SDL_AudioStream*[] streams;
+	private const(ubyte)[][] loadedWAVs;
+	private const(ubyte)[][] playingWAVs;
+	private SDL_AudioStream* mainStream;
 	void initialize(uint sampleRate, uint channels, uint samples) @trusted {
 		this.sampleRate = sampleRate;
 		this.channels = channels;
@@ -30,12 +35,47 @@ class SDL3Audio : AudioBackend {
 	void installCallback(void* user, AudioCallback fun) @trusted {
 		const spec = SDL_AudioSpec(format: SDL_AUDIO_S16, channels: channels, freq: sampleRate);
 		callbackInfo = new AudioCallbackInfo(user, fun);
-	    auto stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, &callbackWrapper, callbackInfo);
-	    SDL_ResumeAudioDevice(SDL_GetAudioStreamDevice(stream));
+		auto dev = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec);
+		mainStream = SDL_CreateAudioStream(&spec, &spec);
+		enforceSDL(!!mainStream, "Could not open audio device");
+		enforceSDL(SDL_SetAudioStreamGetCallback(mainStream, &callbackWrapper, callbackInfo), "Could not set callback");
+		enforceSDL(SDL_BindAudioStream(dev, mainStream), "Could not bind audio stream");
 	}
 	void deinitialize() @safe {}
-	void loadWAV(const ubyte[] data) @safe {}
-	void playWAV(size_t id, int channel) @safe {}
+	void loadWAV(const ubyte[] data) @trusted {
+		ubyte* buf;
+		uint length;
+		SDL_AudioSpec spec;
+		enforceSDL(SDL_LoadWAV_IO(SDL_IOFromMem(cast(void*)&data[0], data.length), true, &spec, &buf, &length), "Couldn't load WAV");
+		loadedWAVs ~= buf[0 .. length];
+	}
+	void playWAV(size_t id, int channel) @trusted {
+		const spec = SDL_AudioSpec(format: SDL_AUDIO_S16, channels: channels, freq: sampleRate);
+		if (channel + 1 > streams.length) {
+			playingWAVs.length = channel + 1;
+			streams.length = channel + 1;
+		}
+		playingWAVs[channel] = loadedWAVs[id];
+		if (streams[channel] == null) {
+			streams[channel] = SDL_CreateAudioStream(&spec, &spec);
+			enforceSDL(SDL_BindAudioStream(SDL_GetAudioStreamDevice(mainStream), streams[channel]), "Could not bind audio stream");
+		} else {
+			// clear callback just in case one was already active
+			enforceSDL(SDL_SetAudioStreamGetCallback(streams[channel], null, null), "Could not clear callback");
+		}
+		enforceSDL(SDL_SetAudioStreamGetCallback(streams[channel], &wavPlayer, &playingWAVs[channel]), "Could not set callback");
+	}
+}
+
+private extern(C) void wavPlayer(void* user, SDL_AudioStream* stream, int additional, int total) nothrow {
+	auto wav = cast(const(ubyte)[]*)user;
+	auto data = (*wav)[0 .. min($, additional)];
+	*wav = (*wav)[data.length .. $];
+	SDL_PutAudioStreamData(stream, &data[0], cast(int)data.length);
+	if (wav.length == 0) {
+		SDL_FlushAudioStream(stream);
+		SDL_SetAudioStreamGetCallback(stream, null, null);
+	}
 }
 
 private extern(C) void callbackWrapper(void* extra, SDL_AudioStream* stream, int additional, int total) nothrow {
